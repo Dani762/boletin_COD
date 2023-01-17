3046bc56c498ca7545c7766ff675addffa789d5c
danielgomezperez <daniobey24@gmail.com>
2023-01-17 19:58:27 +0100

    F

--- git_ramas/Ramas/src/Main.java
+++ git_ramas/Ramas/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("¡Hola mundo!");
         System.out.println("Me llamo Dani y tengo 24 años");
+
+        System.out.println("Todos los cambios");
     }
 }
--- git_ramas/Ramas/src/Prueba.java
+++ git_ramas/Ramas/src/Prueba.java
@@ -2,4 +2,6 @@ public class Prueba {
     int numero = 0;
 
     int numero2 = 5;
+
+    int n3 = 3;
 }
